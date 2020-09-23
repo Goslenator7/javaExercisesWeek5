9949ea73b785a9e9242abe5b7d5e5a46352da1ca
Ryan <rmc7394@gmail.com>
2020-09-23 14:51:15 +0100

    Initial commit, question 1

---
+++ Question1.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class Question1 {
+    public static void main(String[] args) {
+        // Call the welcome() method
+        welcome();
+    }
+
+    // Initialise the welcome() method and message content
+    private static void welcome() {
+        JOptionPane.showMessageDialog(null, "Welcome to my program!");
+    }
+}
